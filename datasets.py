da1a66ef885faf02f7f2cb98c0763c4b9b2f299f
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-11-18 16:19:19 +0100

    Avance Panda csv

--- datasets.py
+++ datasets.py
@@ -2,4 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-print("Marvin inutil")
+
+dfVideoJuegos = pd.read_csv("RA3/VideoGamesSales.csv",sep=";")
+print(dfVideoJuegos.head())
+print("--------------------------------------------------------")
+print(dfVideoJuegos.info)
+print("--------------------------------------------------------")
+print(dfVideoJuegos.describe)
+dfVideoJuegos["release_date"] = pd.to_datetime(dfVideoJuegos["release_date"])
