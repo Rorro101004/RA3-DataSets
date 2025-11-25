c6de603e0c3f9d2148ed40efcc4ac28f9d1b2142
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-11-25 15:28:55 +0100

    commit marvin idiota

--- datasets.py
+++ datasets.py
@@ -9,6 +9,15 @@ print("--------------------------------------------------------")
 #print(dfVideoJuegos.info)
 print("--------------------------------------------------------")
 #print(dfVideoJuegos.describe)
-dfVideoJuegos["release_date"] = pd.to_datetime(dfVideoJuegos["release_date"])
-dfVideoJuegos["last_update"] = pd.to_datetime(dfVideoJuegos["last_update"])
+dfVideoJuegos["release_date"] = pd.to_datetime(dfVideoJuegos["release_date"] ,dayfirst=True, errors="coerce")
+dfVideoJuegos["last_update"] = pd.to_datetime(dfVideoJuegos["last_update"] ,dayfirst=True, errors="coerce")
 print(dfVideoJuegos.head())
+# Conteo de valores nulos por columna
+print("Valores nulos por columna:")
+print(dfVideoJuegos.isna().sum())
+dfVideoJuegos.fillna({'critic_score': 0, 'total_sales': 0}, inplace=True)
+#Corregir formato, coma en vez de punto
+dfVideoJuegos['total_sales'] = (dfVideoJuegos['total_sales'].astype(str).str.replace(',', '.'))
+
+
+
