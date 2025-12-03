56c68416486bd8a8157911265ba310201bb12fa2
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-12-03 16:09:20 +0100

    changes python

--- datasets.py
+++ datasets.py
@@ -20,21 +20,38 @@ print("=== Valores nulos por columna ===")
 print(dfVideoJuegos.isna().sum(), "\n")
 
 # 4. Rellenar nulos en critic_score y total_sales
-dfVideoJuegos = pd.read_csv("RA3-DataSets/VideoGamesSales.csv",sep=";")
-print(dfVideoJuegos.head())
-print("--------------------------------------------------------")
-# 2. print(dfVideoJuegos.info)
-print("--------------------------------------------------------")
-# 2. print(dfVideoJuegos.describe)
-dfVideoJuegos["release_date"] = pd.to_datetime(dfVideoJuegos["release_date"] ,dayfirst=True, errors="coerce")
-dfVideoJuegos["last_update"] = pd.to_datetime(dfVideoJuegos["last_update"] ,dayfirst=True, errors="coerce")
-print(dfVideoJuegos.head())
-# Conteo de valores nulos por columna
-print("Valores nulos por columna:")
-print(dfVideoJuegos.isna().sum())
 dfVideoJuegos.fillna({'critic_score': 0, 'total_sales': 0}, inplace=True)
-#Corregir formato, coma en vez de punto
-dfVideoJuegos['total_sales'] = (dfVideoJuegos['total_sales'].astype(str).str.replace(',', '.'))
+
+# 5. Corregir formato de total_sales (coma → punto → float)
+dfVideoJuegos['total_sales'] = (
+    dfVideoJuegos['total_sales'].astype(str).str.replace(',', '.').astype(float)
+)
+
+# 6. Crear columna de ventas regionales
+dfVideoJuegos['regional_sales_sum'] = (
+    dfVideoJuegos['na_sales'] +
+    dfVideoJuegos['jp_sales'] +
+    dfVideoJuegos['pal_sales'] +
+    dfVideoJuegos['other_sales']
+)
+
+print("=== Comparación total_sales vs regional_sales_sum ===")
+print(dfVideoJuegos[['title', 'total_sales', 'regional_sales_sum']].head(10), "\n")
+
+# 7. Porcentaje de ventas en Norteamérica
+dfVideoJuegos['sales_na_pct'] = dfVideoJuegos['na_sales'] / dfVideoJuegos['total_sales']
+
+print("=== Ventas en Norteamérica (primeras filas) ===")
+print(dfVideoJuegos[['title', 'na_sales', 'total_sales', 'sales_na_pct']].head(10), "\n")
+
+# 8. Edad del juego desde lanzamiento hasta última actualización
+dfVideoJuegos['age_days_since_release'] = (
+    dfVideoJuegos['last_update'] - dfVideoJuegos['release_date']
+).dt.days
+
+print("=== Edad del juego en días (10 filas al azar) ===")
+
+print(dfVideoJuegos[['title', 'release_date', 'last_update',  'age_days_since_release']].sample) 
 
 #=========================================================
 #============ 4. Análisis exploratorio =================== 
